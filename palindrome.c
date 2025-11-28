d65236a5af524355387ec21c0e00e00d1a18c315
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-28 14:22:49 +0530

    Refactor palindrome logic and fix return statements and with functions
    Refactor palindrome check into a separate function and fix logic errors.

--- palindrome.c
+++ palindrome.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
+int pali_n(int);
 int main(){
-int rev=0,n;
+int n;
 printf("Enter a number to reverse:\n");
 scanf("%d",&n);
-while(n>0){ 
+int r=pali_n(n);
+if(r=n){
+printf("The given number/word is palindrome");
+}
+else{ 
+printf("The given number/word is palindrome");
+}
+return 0;
+}
+int pali_n(int n){
+    int rev=0;
+    while(n>0){ 
     rev=rev*10+n%10;
     n=n/10;
+    return rev;
+    }
 }
-printf("The reversed number is: %ch\n",rev);
-if(rev=n)
-printf("The given number/word is palindrome");
-else 
-printf("The given number/word is palindrome");
-return 0;
-
-}
+    
